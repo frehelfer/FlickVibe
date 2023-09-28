710aebb23fe3815c32c9fac78cf86dc2999ef105
Frédéric Helfer <fre.helfer@gmail.com>
2023-09-28 14:57:01 -0300

    Initial Commit

---
+++ FlickVibe/FlickVibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickVibeApp.swift
+//  FlickVibe
+//
+//  Created by Frédéric Helfer on 28/09/23.
+//
+
+import SwiftUI
+
+@main
+struct FlickVibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
